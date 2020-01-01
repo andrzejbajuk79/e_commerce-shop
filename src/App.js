9e13ece5963707c13d5ac6a208d24a25988a96eb
ptasiek3003@gmail.com <ptasiek3003@gmail.com>
2020-01-01 19:57:15 +0100

    przekierowanie na glowna strone/blokada Loginu po zalogowaniu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-import Loging from './pages/loging/loging.component';
+ import Loging from './pages/loging/loging.component';
 import Header from './components/header/header.component.jsx';
 import { setCurrentUser } from './redux/user/user.action';
 
@@ -25,7 +25,7 @@ class App extends React.Component {
 					});
 				});
 			} else {
-			setCurrentUser(userAuth);
+				setCurrentUser(userAuth);
 			}
 		});
 	}
@@ -42,13 +42,23 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
-					<Route path="/signing" component={Loging} />
+					{/* <Route path="/signing" component={Loging} /> */}
+					<Route
+						path="/signing"
+						render={() =>
+							this.props.currentUser ? <Redirect to="/" /> : <Loging />
+						}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
+const mapStateToProps =  ({user})  => ({ 
+	currentUser: user.currentUser 
+});
+
 const mapDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect( mapStateToProps,mapDispatchToProps)(App);
